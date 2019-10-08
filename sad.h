e17df4483cd206915a892d7e211d9c22cf64791f
konfiot <arthur.toussaint@polytechnique.edu>
2019-10-07 23:46:21 -0400

    Petite avancée

--- sad.h
+++ sad.h
@@ -8,12 +8,25 @@
 
 SC_MODULE(sad)
 {
-	sc_in<unsigned int> A, B;
-	sc_out<unsigned int> F;
-
 	void do_sad()
 	{
-		F.write( !(A.read() && B.read()) );
+		int i, v;
+		unsigned int block;
+		unsigned int sad;
+		unsigned int a, b;
+
+		for (block=0; block<NUM_BLOCKS; block++) {
+			sad = 0;
+			for (i=0; i<BLOCK_SIZE; i++) {
+				MEM.Read(INPUT1_ADDR+(block*BLOCK_SIZE)+i, &a);
+				MEM.Read(INPUT2_ADDR+(block*BLOCK_SIZE)+i, &b);
+
+				v = a - b;
+				if( v < 0 ) v = -v;
+				sad += v;
+			}
+			MEM.Write(SAD_OUTPUT_ADDR + block, sad);
+		}
 	}
 
 	SC_CTOR(nand2)
