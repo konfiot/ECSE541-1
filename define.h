3562ac67072178ec50855d304bcb5fc22505d5a6
konfiot <arthur.toussaint@polytechnique.edu>
2019-10-08 12:25:09 -0400

    Define MEM_SIZE

--- define.h
+++ define.h
@@ -1,5 +1,9 @@
+#define max(a,b) ((a) &gt; (b) ? (a) : (b))
+#define max3(a,b,c) (max((a), max((b),(c))))
+
 #define NUM_BLOCKS 64
 #define BLOCK_SIZE 64
 #define INPUT1_ADDR 0
 #define INPUT2_ADDR 16384
 #define SAD_OUTPUT_ADDR 32768
+#define MEM_SIZE NUM_BLOCKS*BLOCK_SIZE+ max3(SAD_OUTPUT_ADDR, INPUT1_ADDR, INPUT2_ADDR)
--- mem_if.c
+++ mem_if.c
@@ -2,13 +2,10 @@
 #include "mem_if.h"
 #include "define.h"
 
-#define max(a,b) ((a) &gt; (b) ? (a) : (b))
-#define max3(a,b,c) (max((a), max((b),(c))))
-
 class simple_mem : public sc_module, public simple_mem_if
 {
 	private:
-		unsigned int data[NUM_BLOCKS*BLOCK_SIZE+ max3(SAD_OUTPUT_ADDR, INPUT1_ADDR, INPUT2_ADDR)];
+		unsigned int data[MEM_SIZE];
 
 	public:
 		// constructor
