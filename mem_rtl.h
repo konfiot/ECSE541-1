806e7207dcb8bdf710b1a2a8752083985fef0993
Juan-Ca <juan.borges@mail.mcgill.ca>
2019-10-11 01:34:58 -0400

    Moves the RTL function inside the class definition

--- mem_rtl.h
+++ mem_rtl.h
@@ -19,10 +19,7 @@ public:
 
   SC_HAS_PROCESS(MEMORY_RTL);
 
-  void rtl();
-
-  MEMORY_RTL(sc_module_name nm, char* filename) :
-    sc_module(nm)
+  MEMORY_RTL(sc_module_name nm, char* filename) : sc_module(nm)
   {
     // Initialize memory
     unsigned int data, i = 0;
@@ -44,39 +41,39 @@ public:
     SC_METHOD(rtl);
     sensitive << clk.pos();
   }
-};
 
-void MEMORY_RTL::rtl()
-{
-  /*
-    if ren : output data at addr on dataOut
-    if wen : write data at addr
-    if addr is valid : ack is
-  */
+  void rtl()
+  {
+    /*
+      if ren : output data at addr on dataOut
+      if wen : write data at addr
+      if addr is valid : ack is
+    */
 
-  // RW functions
-  // address valid
-  sc_uint<32> addr_val = addr.read();
-  if (addr_val < MEM_SIZE)
-  {
-    // Read mode
-    if(ren.read() == sc_logic_1)
+    // RW functions
+    // address valid
+    sc_uint<32> addr_val = addr.read();
+    if (addr_val < MEM_SIZE)
     {
-      dataOut.write(mem[addr_val]);
+      // Read mode
+      if(ren.read() == sc_logic_1)
+      {
+        dataOut.write(mem[addr_val]);
+      }
+      // Write mode
+      else if (wen.read() == sc_logic_1)
+      {
+        mem[addr_val] = dataIn.read();
+      }
+      // address valid
+      ack.write(sc_logic_1);
     }
-    // Write mode
-    else if (wen.read() == sc_logic_1)
+    // address invalid
+    else
     {
-      mem[addr_val] = dataIn.read();
+      ack.write(sc_logic_0);
     }
-    ack.write(sc_logic_1);
   }
-  // address invalid
-  else
-  {
-    ack.write(sc_logic_0);
-  }
-}
 };
 
 #endif //__MEMORY_RTL_H__
